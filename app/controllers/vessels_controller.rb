409a7a048523f91b76d9bc3bd19cf5d2077e9aeb
Błażej Kosmowski <b.kosmowski@selleo.com>
2022-04-21 15:18:27 +0200

    Fix fake data display on vessel dashboard

--- app/controllers/vessels_controller.rb
+++ app/controllers/vessels_controller.rb
@@ -1,20 +1,17 @@
 class VesselsController < BaseController
-
   def show
     super do
-      @trackable_metrics = resource.vessel_parameters.map do |vessel_parameter|
+      @trackable_metrics = resource.vessel_system_parameters.map do |vessel_system_parameter|
         values = (0..10).to_a.reverse.each_with_object({}) do |index, result|
           result[(Date.current - index.days).iso8601] = rand(100)
         end
 
         OpenStruct.new(
-          name: vessel_parameter.parameter.label,
+          name: vessel_system_parameter.parameter.label,
           last_value: values[Date.current.iso8601],
           trend: values
         )
       end
     end
   end
-  protected
-
 end
