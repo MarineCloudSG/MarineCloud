42282ece8a38d30371e751e6b2db51085a48b6e9
Rafal Jagoda <rafaljagoda@gmail.com>
2022-05-23 11:16:28 +0200

    update base color

--- app/components/sortable_list_component.rb
+++ app/components/sortable_list_component.rb
@@ -15,7 +15,7 @@ class SortableListComponent < ViewComponent::Base
 
   def move_up_link
     link_to helpers.public_send("#{@route}_path", @resource.position, new_position: @resource.position - 1),
-            class: "inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+            class: "inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500",
             data: { 'turbo-method' => :put } do
       helpers.heroicon("chevron-up", options: { class: "h-5 w-5", disable_default_class: true })
     end
@@ -23,7 +23,7 @@ class SortableListComponent < ViewComponent::Base
 
   def move_down_link
     link_to helpers.public_send("#{@route}_path", @resource.position, new_position: @resource.position + 1),
-            class: "inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+            class: "inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500",
             data: { 'turbo-method' => :put } do
       helpers.heroicon("chevron-down", options: { class: "h-5 w-5", disable_default_class: true })
     end
--- app/components/tailwindui/form/select_component.rb
+++ app/components/tailwindui/form/select_component.rb
@@ -3,7 +3,7 @@
 class TailwindUI::Form::SelectComponent < ViewComponent::Form::SelectComponent
   def html_class
     class_names(
-      "mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 shadow-sm sm:text-sm rounded-md",
+      "mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full border-gray-300 shadow-sm sm:text-sm rounded-md",
       "border-red-300 text-red-900 placeholder-red-300": method_errors?
     )
   end
