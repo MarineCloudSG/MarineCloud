8f87e87cb36c6e8d54ab06d8b95a267a983c0024
Igor Kowasz <igikoval@gmail.com>
2022-06-14 11:43:55 +0200

    [HOTFIX] bumping migration number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 202204020181770) do
+ActiveRecord::Schema.define(version: 202204020181771) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
