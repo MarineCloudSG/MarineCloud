906c9b45cead517921f1dce2a15f57b58b8217d8
Igor Kowasz <igikoval@gmail.com>
2022-06-14 18:55:55 +0200

    [#181998305] upload reminders target email expanded

--- app/mailers/vessel_mailer.rb
+++ app/mailers/vessel_mailer.rb
@@ -2,6 +2,6 @@ class VesselMailer < ApplicationMailer
   def data_import_reminder_email
     @vessel = params[:vessel]
     @url = "/vessels/#{@vessel.id}"
-    mail(to: @vessel.user.email, subject: "Reminder // Boiler and Cooling water analysis data submission")
+    mail(to: [@vessel.user.email, @vessel.email], subject: "Reminder // Boiler and Cooling water analysis data submission")
   end
 end
