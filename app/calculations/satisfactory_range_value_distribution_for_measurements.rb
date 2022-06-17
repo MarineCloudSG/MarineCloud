d6d4ebda74f314f13c66f4810afb08d5b78ebaac
Igor Kowasz <igikoval@gmail.com>
2022-06-17 04:41:53 +0200

    [#182221801] fixed measurements percentage values

--- app/calculations/satisfactory_range_value_distribution_for_measurements.rb
+++ app/calculations/satisfactory_range_value_distribution_for_measurements.rb
@@ -3,26 +3,33 @@ class SatisfactoryRangeValueDistributionForMeasurements < Patterns::Calculation
   private
 
   def result
-    applicable_value_states.map { |s| [s, state_percentage(s)] }.to_h
+    applicable_states.map { |state| [state, states_percentages[state] || 0] }.to_h
   end
 
-  def state_percentage(state)
-    state_values_count(state).to_f / measurements.count * 100
+  def applicable_states
+    states = [:in_range]
+    states << :overrange unless highest_satisfactory_range.nil?
+    states << :underrange unless lowest_satisfactory_range.nil?
+    states
   end
 
-  def state_values_count(state)
-    measurements.select { |row| row.state.to_sym == state }.count
+  def states_percentages
+    @states_percentages ||= states_values_counts.transform_values { |count| count.to_f / measurements.count * 100 }
   end
 
-  def applicable_value_states
-    Measurement.states.keys.map(&:to_sym).select { |s| state_applicable?(s) }
+  def states_values_counts
+    measurements_with_value_states.group_by(&:state).transform_values(&:count)
   end
 
-  def state_applicable?(state)
-    return false if state == :underrange && lowest_satisfactory_range.nil?
-    return false if state == :overrange && highest_satisfactory_range.nil?
+  def measurements_with_value_states
+    measurements.map { |m| OpenStruct.new(state: measurement_value_state(m), measurement: m) }
+  end
 
-    true
+  def measurement_value_state(measurement)
+    return :underrange if !lowest_satisfactory_range.nil? && measurement.value < lowest_satisfactory_range
+    return :overrange if !highest_satisfactory_range.nil? && measurement.value > highest_satisfactory_range
+
+    :in_range
   end
 
   def lowest_satisfactory_range
--- spec/calculations/satisfactory_range_value_distribution_for_measurements_spec.rb
+++ spec/calculations/satisfactory_range_value_distribution_for_measurements_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe SatisfactoryRangeValueDistributionForMeasurements do
       vsp = create :vessel_system_parameter, min_satisfactory: 0.5, max_satisfactory: 1.5
       import = create :measurements_import
       measurements = [
-        (create :measurement, state: :in_range, value: 1, vessel_system_parameter: vsp, measurements_import: import),
-        (create :measurement, state: :in_range, value: 1, vessel_system_parameter: vsp, measurements_import: import),
-        (create :measurement, state: :in_range, value: 1, vessel_system_parameter: vsp, measurements_import: import),
-        (create :measurement, state: :overrange, value: 1.6, vessel_system_parameter: vsp, measurements_import: import),
-        (create :measurement, state: :underrange, value: 0.2, vessel_system_parameter: vsp, measurements_import: import)
+        (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+        (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+        (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+        (create :measurement, value: 1.6, vessel_system_parameter: vsp, measurements_import: import),
+        (create :measurement, value: 0.2, vessel_system_parameter: vsp, measurements_import: import)
       ]
 
       stats = SatisfactoryRangeValueDistributionForMeasurements.result_for(measurements)
@@ -18,6 +18,96 @@ RSpec.describe SatisfactoryRangeValueDistributionForMeasurements do
       expect(stats[:in_range]).to eq(60)
       expect(stats[:underrange]).to eq(20)
       expect(stats[:overrange]).to eq(20)
+    end
+
+    context 'there is no min' do
+      it 'counts correct statistics for given measurements' do
+        vsp = create :vessel_system_parameter, min_satisfactory: nil, max_satisfactory: 1.5
+        import = create :measurements_import
+        measurements = [
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1.6, vessel_system_parameter: vsp, measurements_import: import),
+        ]
+
+        stats = SatisfactoryRangeValueDistributionForMeasurements.result_for(measurements)
+
+        expect(stats.count).to eq(2)
+        expect(stats[:in_range]).to eq(80)
+        expect(stats[:overrange]).to eq(20)
+      end
+    end
+
+
+    context 'there is no max' do
+      it 'counts correct statistics for given measurements' do
+        vsp = create :vessel_system_parameter, min_satisfactory: 0.5, max_satisfactory: nil
+        import = create :measurements_import
+        measurements = [
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+          (create :measurement, value: 0.3, vessel_system_parameter: vsp, measurements_import: import),
+        ]
+
+        stats = SatisfactoryRangeValueDistributionForMeasurements.result_for(measurements)
+
+        expect(stats.count).to eq(2)
+        expect(stats[:in_range]).to eq(80)
+        expect(stats[:underrange]).to eq(20)
+      end
+    end
+
+    context 'values are set in chemical program' do
+      context 'are not overriden' do
+        it 'takes them' do
+          program = create :chemical_program
+          system = create :vessel_system, chemical_program: program
+          chemparam = create :chemical_program_parameter, min_satisfactory: 0.4, max_satisfactory: 1.5, system: system.system, chemical_program: program
+          vsp = create :vessel_system_parameter, min_satisfactory: nil, max_satisfactory: nil, parameter: chemparam.parameter, vessel_system: system
+          import = create :measurements_import
+          measurements = [
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1.7, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 0.3, vessel_system_parameter: vsp, measurements_import: import),
+          ]
+
+          stats = SatisfactoryRangeValueDistributionForMeasurements.result_for(measurements)
+
+          expect(stats.count).to eq(3)
+          expect(stats[:in_range]).to eq(60)
+          expect(stats[:underrange]).to eq(20)
+          expect(stats[:overrange]).to eq(20)
+        end
+      end
+
+      context 'are overriden' do
+        it 'ignores them' do
+          program = create :chemical_program
+          system = create :vessel_system, chemical_program: program
+          chemparam = create :chemical_program_parameter, min_satisfactory: 0.4, max_satisfactory: 1.5, system: system.system, chemical_program: program
+          vsp = create :vessel_system_parameter, min_satisfactory: 0.5, max_satisfactory: nil, parameter: chemparam.parameter, vessel_system: system
+          import = create :measurements_import
+          measurements = [
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 1.8, vessel_system_parameter: vsp, measurements_import: import),
+            (create :measurement, value: 0.3, vessel_system_parameter: vsp, measurements_import: import),
+          ]
+
+          stats = SatisfactoryRangeValueDistributionForMeasurements.result_for(measurements)
+
+          expect(stats.count).to eq(2)
+          expect(stats[:in_range]).to eq(80)
+          expect(stats[:underrange]).to eq(20)
+        end
+      end
     end
   end
 end
