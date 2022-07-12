84f02ed4a610c90f17fb66cbfab3dd1619bfdedd
Igor Kowasz <igikoval@gmail.com>
2022-07-12 19:11:53 +0200

    [HOTFIX] migration duplicated number fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 202204020181775) do
+ActiveRecord::Schema.define(version: 202204020181776) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
